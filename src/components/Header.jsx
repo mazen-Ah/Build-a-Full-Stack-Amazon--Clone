13c9ca559f26801acab5d79a3fb36d5acfcf094a
kirion13 <kirioncore@gmail.com>
2023-01-10 23:10:48 +0200

    create header

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="bg-[#131921] w-full h-2">
+      <div>
+        <img
+          src="https://clone-dffaf.web.app/static/media/amazonLogo.1c9be0b254d3ff1dc9e9.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
